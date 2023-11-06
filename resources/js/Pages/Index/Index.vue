bdee35e58afed9f3d8794366e60f3c468cf5a493
webflyx <zavala180395@gmail.com>
2023-11-06 15:04:19 +0200

    Tests views/layout

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Hello vue</div>
+    <Link href="/show">Link to show</Link>
+    <div>{{ message }}</div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+defineProps({
+    message: String
+})
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,8 @@
+<template>
+    <div> Show vue</div>
+    <Link href="/">Link to index</Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
