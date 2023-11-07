7ba791ce14fd42951dcc551c2b156f8949ddd4db
webflyx <zavala180395@gmail.com>
2023-11-07 19:37:34 +0200

    Added Listing migrate, module, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+
+        Listing::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
