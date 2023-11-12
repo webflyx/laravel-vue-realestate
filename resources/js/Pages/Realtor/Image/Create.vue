0aa490967007d9ab4b433d00c5ea5d0b349917cb
webflyx <zavala180395@gmail.com>
2023-11-12 15:06:09 +0200

    Added upload images logic via frontend

--- resources/js/Pages/Realtor/Image/Create.vue
+++ resources/js/Pages/Realtor/Image/Create.vue
@@ -1,16 +1,44 @@
 <template>
   <Box>
-    <form method="POST" enctype="multipart/form-data" :action="route('realtor.listing.image.store', listing.id)">
-      <input type="file" multiple name="images[]" />
-      <button type="submit" class="btn-primary">Save</button>
+    <template #header>Upload new images</template>
+    <form @submit.prevent="upload">
+      <input type="file" multiple @input="addFiles" />
+      <div class="mt-4 flex gap-2">
+        <button type="submit" class="btn-primary">Upload</button>
+        <button type="reset" class="btn-second" @click="reset">Reset</button>
+      </div>
     </form>
   </Box>
 </template>
 
 <script setup>
 import Box from '@/Components/UI/Box.vue'
+import { useForm } from '@inertiajs/vue3'
 
-defineProps({
+const props = defineProps({
   listing: Object,
 })
+
+const form =  useForm({
+  images: [],
+})
+
+const upload = () => {
+  form.post(
+    route('realtor.listing.image.store', props.listing.id),
+    {
+      onSuccess: () => form.reset('images'),
+    },
+  )
+}
+
+const addFiles = (event) => {
+  for (const image of event.target.files){
+    form.images.push(image)
+  }
+}
+
+const reset = () => form.reset('images')
+ 
+
 </script>
